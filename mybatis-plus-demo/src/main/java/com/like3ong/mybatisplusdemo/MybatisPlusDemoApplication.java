a9ca60c55b71601a302fdf5adf4c0fc23815dd04
likesong <1058725269@qq.com>
2021-09-29 16:00:59 +0800

    Mybatis-plus学习记录

---
+++ mybatis-plus-demo/src/main/java/com/like3ong/mybatisplusdemo/MybatisPlusDemoApplication.java
@@ -0,0 +1,16 @@
+package com.like3ong.mybatisplusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 10587
+ */
+@SpringBootApplication
+public class MybatisPlusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusDemoApplication.class, args);
+    }
+
+}
---
+++ mybatis-plus-demo/src/test/java/com/like3ong/mybatisplusdemo/MybatisPlusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.like3ong.mybatisplusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
