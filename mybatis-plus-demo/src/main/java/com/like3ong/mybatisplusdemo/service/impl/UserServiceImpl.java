51b4045cf4a160e91667a90759f03a3ef79e207e
likesong <1058725269@qq.com>
2021-10-02 12:54:01 +0800

    测试分支开发-切分bug分支

--- mybatis-plus-demo/src/main/java/com/like3ong/mybatisplusdemo/service/impl/UserServiceImpl.java
+++ mybatis-plus-demo/src/main/java/com/like3ong/mybatisplusdemo/service/impl/UserServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 /**
  *
  * @author 10587
- * 这是一个测试，测试1
+ * 这是一个测试，测试1,bug分支
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
